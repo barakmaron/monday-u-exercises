34d028f771c777d914def54774082197de0f3916
Barak Maron <Barak062@gmail.com>
2022-06-23 19:16:05 +0300

    feat: adds edit for task

--- src/ex4/dist/services/DomManager.js
+++ src/ex4/dist/services/DomManager.js
@@ -1,4 +1,4 @@
-import { LOADER, POKEMON_IMAGE_DIV, TASK_HTML, ERROR } from '../config/GlobalConses.js';
+import { LOADER, POKEMON_IMAGE_DIV, TASK_HTML, ERROR, EDIT_TASK } from '../config/GlobalConses.js';
 
 export default class DomManager {
   constructor() {
@@ -15,24 +15,31 @@ export default class DomManager {
     this.task_input.value = '';
   }
 
-  AddNewTask(id, task_object, delete_call_back, complete_call_back) {
+  AddNewTask(task_object, delete_call_back, complete_call_back, edit_call_back) {
     const new_task_from_html = this.CreateElementFromHtml(TASK_HTML);
     const task_number = new_task_from_html.querySelector('.task_number');
     const task_text = new_task_from_html.querySelector('.task_text');
+    const edit_task_button = new_task_from_html.querySelector('.edit_task_button');
     const task_delete_button = new_task_from_html.querySelector('.delete_task_button');
-    const task_complete_button = new_task_from_html.querySelector('.complete_task_button');
-    task_delete_button.id = id;
-    task_complete_button.id = id;
+    const task_complete_checkbox = new_task_from_html.querySelector('.checkbox_complete');
+    task_delete_button.id = task_object.id;
+    task_complete_checkbox.id = task_object.id;
+    edit_task_button.id = task_object.id;
 
-    if (task_object.completed) { new_task_from_html.classList.add('completed_task'); }
+    if (task_object.status) 
+      task_complete_checkbox.checked = true; 
     // Delete empty state from task container
     this.task_container.classList.remove('empty');
-    if (Number.isInteger(task_object.id)) {
-      task_text.after(this.PokemonImageElement(task_object));
+    if (task_object.is_pokemon) {
+      task_text.parentNode.after(this.PokemonImageElement(task_object.PokemonDatum));
       // adds text to task
-      task_text.innerHTML = `Catch ${task_object.name}`;
-    } else
-      task_text.innerHTML = task_object.name;
+      task_text.innerHTML = `Catch ${task_object.ItemName}`;
+      // disable edit of pokemon
+      edit_task_button.setAttribute('disabled', '');
+    } else {
+      task_text.innerText = task_object.ItemName;
+    }
+    task_text.addEventListener('click', this.TaskClick);
     // adds numbers to tasks
     task_number.innerText = (this.task_container.children.length + 1).toString().concat(')');
     this.clearInput();
@@ -40,11 +47,14 @@ export default class DomManager {
     task_delete_button.addEventListener('click', (event) => {
       delete_call_back(event);
     });
-    task_complete_button.addEventListener('click', (event) => {
+    task_complete_checkbox.addEventListener('click', (event) => {
       this.MarkAsCompleteTask(event);
       complete_call_back(event);
     });
-    task_text.addEventListener('click', this.TaskClick);
+    edit_task_button.addEventListener('click', (event) => {
+      this.EditTask(event, task_text, edit_call_back);
+      //edit_call_back(event);
+    });
     this.task_container.append(new_task_from_html);
     // updates task counter
     this.UpdateTaskCounter();
@@ -52,17 +62,18 @@ export default class DomManager {
 
   PokemonImageElement(task_object) {
     const pokemon_image_sprites_container = this.CreateElementFromHtml(POKEMON_IMAGE_DIV);
-    Object.entries(task_object.images).forEach((image_attribute, index) => {
+    Object.entries(task_object.PokemonImages).forEach((image, index) => {
+      const [, image_object] = image;
       // adds all images from api
       const pokemon_image = document.createElement('img');
-      pokemon_image.id = index;
+      pokemon_image.id = image_object.id;
       // adds class for animation
       if (index === 0) 
         pokemon_image.classList.add('show');
       else 
         pokemon_image.classList.add('hide'); 
       pokemon_image.classList.add('pokemon_image');
-      [, pokemon_image.src] = image_attribute;
+      pokemon_image.src = image_object.image;
       pokemon_image_sprites_container.appendChild(pokemon_image);
     });
     return pokemon_image_sprites_container;
@@ -76,7 +87,7 @@ export default class DomManager {
   }
 
   MarkAsCompleteTask(event) {
-    event.currentTarget.parentNode.parentNode.classList.toggle('completed_task');
+    event.currentTarget.checked = !event.currentTarget.checked;
     this.UpdateTaskCounter();
   }
 
@@ -87,7 +98,7 @@ export default class DomManager {
   UpdateTaskCounter() {
     const tasks_number = this.task_container.children.length;
     // takes in consideration task completed
-    const task_completed = this.task_container.querySelectorAll('.completed_task').length;
+    const task_completed = this.task_container.querySelectorAll('.checkbox_complete:checked').length;
     this.tasks_counter.innerHTML = tasks_number - task_completed;
   }
 
@@ -100,13 +111,13 @@ export default class DomManager {
     this.task_input.setCustomValidity('');
   }
 
-  RenderDomFromArray(todos, delete_call_back, complete_call_back) {
+  RenderDomFromArray(todos, delete_call_back, complete_call_back, edit_call_back) {
     const todos_copy = [...todos];
     // clear task container for rendering
     this.task_container.innerHTML = '';
     if (todos_copy.length) 
       todos_copy.map((todo, key) => {
-        this.AddNewTask(key, todo, delete_call_back, complete_call_back);
+        this.AddNewTask(todo, delete_call_back, complete_call_back, edit_call_back);
       });
     else 
       this.task_container.classList.add('empty'); 
@@ -170,5 +181,16 @@ export default class DomManager {
   {
     const error = document.querySelector('.error');
     error.remove();
+  }
+
+  EditTask(event, span, call_back_on_edit){
+    const edit_text_container = this.CreateElementFromHtml(EDIT_TASK);
+    const input = edit_text_container.querySelector('#edit_text_input');
+    const conform_button = edit_text_container.querySelector('.complete_edit_task_button');
+    conform_button.id = event.currentTarget.id;    
+    span.after(edit_text_container);
+    input.setAttribute('placeholder', span.innerText);
+    conform_button.addEventListener('click', (event) => call_back_on_edit(event));
+    input.focus();
   }
 }
