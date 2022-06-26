30b737fc86f6c9426a769880df3ae240f0eba18a
Barak Maron <Barak062@gmail.com>
2022-06-26 13:42:51 +0300

    feat: adds task componnent

---
+++ src/client/src/components/Task/Task.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from  'prop-types';
+import style from './task.module.css';
+import pokemon_style from './task_pokemon.module.css';
+import Button from '../Button/Button';
+
+const Task = ({  number, item }) => {
+  const text = item.is_pokemon ? `Catch ${item.ItemName}` : item.ItemName;
+  //checkbox
+  return (
+    <li id={item.id} className={`${style.todo_task}`}>
+      <div className={`${style.checkbox_container}`}></div>
+      <div className={`${style.task_text_container}`}>
+        <span className={`${style.task_number}`}>
+          {number})
+        </span>
+        <span className={`${style.task_text}`}>
+          {text}
+        </span>
+      </div>
+      {item.is_pokemon && <div className={pokemon_style.image_sprites}> 
+        {item.PokemonDatum.PokemonImages.map((image, index) => {
+          const show_hide_class = index === 0 ? pokemon_style.show : pokemon_style.hide;
+          return(
+            <img 
+            alt={item.ItemName}
+            className={`${pokemon_style.pokemon_image} ${show_hide_class}`}
+            key={`pokemon_id${item.PokemonDatum.id}image_id${image.id}`} 
+            src={image.image}/>
+          );
+        })}
+      </div>}
+      <div className={`${style.task_buttons}`}>
+        {!item.is_pokemon && <Button size='small' icon='pencil' background_color='blue'></Button>}
+        <Button size='small' icon='trash' background_color='red'></Button>
+      </div>
+    </li>
+  )
+};
+
+Task.prototype = {
+    item: PropTypes.object,
+    number: PropTypes.number
+}; 
+
+Task.defaultProps = {
+    number: 1
+};
+
+export default Task;
---
+++ src/client/src/components/Task/__stories__/task.stories.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import  Task  from '../Task';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    
+  },
+};
+
+const Template = (args) => <Task {...args} />;
+const item_pokemon = {
+  "id": 1,
+  "ItemName": "bulbasaur",
+  "status": false,
+  "is_pokemon": true,
+  "createdAt": "2022-06-21T13:04:45.000Z",
+  "updatedAt": "2022-06-23T14:46:49.000Z",
+  "PokemonDatum": {
+    "id": 1,
+    "items_id": 1,
+    "pokemon_id": 1,
+    "createdAt": "2022-06-21T13:04:45.000Z",
+    "updatedAt": "2022-06-21T13:04:45.000Z",
+    "PokemonImages": [
+      {
+        "id": 4,
+        "pokemon_id": 1,
+        "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+        "createdAt": "2022-06-21T13:04:45.000Z",
+        "updatedAt": "2022-06-21T13:04:45.000Z"
+      },
+      {
+        "id": 3,
+        "pokemon_id": 1,
+        "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        "createdAt": "2022-06-21T13:04:45.000Z",
+        "updatedAt": "2022-06-21T13:04:45.000Z"
+      },
+      {
+        "id": 2,
+        "pokemon_id": 1,
+        "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png",
+        "createdAt": "2022-06-21T13:04:45.000Z",
+        "updatedAt": "2022-06-21T13:04:45.000Z"
+      },
+      {
+        "id": 1,
+        "pokemon_id": 1,
+        "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+        "createdAt": "2022-06-21T13:04:45.000Z",
+        "updatedAt": "2022-06-21T13:04:45.000Z"
+      }
+    ]
+  }
+};
+
+const item_regular = {
+  "id": 28,
+  "ItemName": "clean code",
+  "status": false,
+  "is_pokemon": false,
+  "createdAt": "2022-06-23T16:08:00.000Z",
+  "updatedAt": "2022-06-23T16:08:00.000Z",
+  "PokemonDatum": null
+};
+
+export const Regular = Template.bind({});
+Regular.args = {
+  number: 1,
+  item: item_regular
+};
+
+export const Pokemon = Template.bind({});
+Pokemon.args = {
+  number: 1,
+  item: item_pokemon
+};
