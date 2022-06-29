5462e8fee7871d28273fbaa7128a1365bf8b3f73
Barak Maron <Barak062@gmail.com>
2022-06-29 15:54:43 +0300

    feat: adds axios requests

---
+++ src/client/src/Api/ApiManger.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+
+const base_url = 'http://127.0.0.1:8000';
+export async function GetResourceRequest(url) {
+    return await axios.get(`${base_url}/${url}`).then((response) => {
+        return response.data.tasks;
+    }).catch((error) => {
+        throw CreateError(error.response.status, error.response.data.error);
+    });
+  }
+  
+export async function AddNewResourceRequest(url, data) {
+    return await axios.post(`${base_url}/${url}`, data).then((response) => {
+        return response.data.tasks;
+    }).catch((error) => {
+        throw CreateError(error.response.status, error.response.data.error);
+    });
+}
+  
+export async function DeleteResourceRequest(url) {
+    return await axios.delete(`${base_url}/${url}`).then((response) => {
+        return response.data.tasks;
+    }).catch((error) => {
+        throw CreateError(error.response.status, error.response.data.error);
+    });
+}
+  
+  export async function PatchResourceRequest(url) {
+    return await axios.patch(`${base_url}/${url}`).then((response) => {
+        return response.data.tasks;
+    }).catch((error) => {
+        throw CreateError(error.response.status, error.response.data.error);
+    });
+  }
+  
+  export async function PutResourceRequest(url, data) {
+    try {
+      const response = await fetch(`${base_url}/${url}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      });
+      // check if response is valid
+      if (!response.ok) {
+        throw new Error(`Cant patch ${url}`);
+      }
+      // parse response to json object
+      const res_obj = await response.json();
+      return res_obj;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  function CreateError(code, message)
+  {
+    const error = new Error(message);
+    error.statusCode = code;
+    return error;
+  }
--- src/client/src/components/CheckBox/CheckBox.jsx
+++ src/client/src/components/CheckBox/CheckBox.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from  'prop-types';
 import style from './checkbox.module.css';
 
-const CheckBox = ({ id, state }) => {
-  const checked = state ? 'checked' : ''
+const CheckBox = ({ id, state, on_click }) => {
+  const [checked, setChecked] = useState(state);
+  function handleChange(){
+    setChecked((value) => !value);
+  }
   return (
     <label className={style.checkbox_label}>
-      <input type="checkbox" className={style.checkbox_complete} value="" id={id}
-      checked={checked}></input>
+      <input 
+      type="checkbox" 
+      className={style.checkbox_complete} 
+      value="" 
+      id={id}
+      onChange={handleChange}
+      checked={checked} 
+      onClick={on_click}></input>
       <span className={style.checkbox_mark}></span>
     </label>
   )
@@ -15,7 +24,8 @@ const CheckBox = ({ id, state }) => {
 
 CheckBox.prototype = {
     id: PropTypes.number,
-    state: PropTypes.bool
+    state: PropTypes.bool,
+    on_click: PropTypes.func
 }; 
 
 CheckBox.defaultProps = {
