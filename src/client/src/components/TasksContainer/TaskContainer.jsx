980a2cfcce534742d5bb1c116b4699ab2d161a53
Barak Maron <Barak062@gmail.com>
2022-06-29 15:58:05 +0300

    feat: adds error hook and done tooltip

--- src/client/src/components/TasksContainer/TaskContainer.jsx
+++ src/client/src/components/TasksContainer/TaskContainer.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import PropTypes from  'prop-types';
 import Task from '../Task/Task';
 import style from './taskcontainer.module.css';
+import { Tooltip } from 'monday-ui-react-core';
 
-const TaskContainer = ({ tasks }) => {
+const TaskContainer = ({ tasks, delete_call, complete_call, edit_call }) => {
   return (
-    <ul id={style.todo_tasks_container} className={!tasks.length && style.empty}>
+    <ul id={style.todo_tasks_container} className={tasks.length ? undefined : style.empty}>
       {tasks.map((task, index) => {
+        const content = (task.done && task.status) ? `Was set as done at: ${task.done}` : `This item is not done it...`;
         return(
-          <Task
-          key={`task_id${task.id}`}
-          number={index + 1}
-          item={task}></Task>
+          <Tooltip 
+            key={`task_id${task.id}tooltip`}
+            immediateShowDelay={0} 
+            content={content}>
+            <Task
+            key={`task_id${task.id}`}
+            number={index + 1}
+            item={task}
+            delete_call={() => delete_call(task.id)}
+            complete_call={() => complete_call(task.id)}
+            edit_call={edit_call}></Task>
+          </Tooltip>
         );
       })}
     </ul>
@@ -19,11 +29,17 @@ const TaskContainer = ({ tasks }) => {
 };
 
 TaskContainer.prototype = {
-    tasks: PropTypes.array
+    tasks: PropTypes.array,
+    delete_call: PropTypes.func,
+    complete_call: PropTypes.func,
+    edit_call: PropTypes.func
 }; 
 
 TaskContainer.defaultProps = {
-    tasks: []
+    tasks: [],
+    delete_call: null,
+    complete_call: null,
+    edit_call: null
 };
 
 export default TaskContainer;
