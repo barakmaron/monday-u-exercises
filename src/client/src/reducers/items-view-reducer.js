273d44de3e86a3a1f949dcb6a06d5d902a2605ff
Barak Maron <Barak062@gmail.com>
2022-07-05 09:57:31 +0300

    feat: adds view reducers

--- src/client/src/reducers/items-view-reducer.js
+++ src/client/src/reducers/items-view-reducer.js
@@ -1,10 +1,19 @@
 import actionTypes from "../actions/constants";
 
 const initialState = {
+  successful: '',
+  failed: '',
+  is_loading: false
 };
 
 const itemsViewReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SUCCESSFUL:
+      return { ...state, successful: action.payload };
+    case actionTypes.FAILED:
+      return { ...state, failed: action.payload };
+    case actionTypes.LOADING:
+      return { ...state, is_loading: action.payload };
     default:
       return state;
   }
