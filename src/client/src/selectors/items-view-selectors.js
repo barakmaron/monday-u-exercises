ecbeac9261a86e7e02a6f350f60ef96789053f97
Barak Maron <Barak062@gmail.com>
2022-07-05 09:58:11 +0300

    feat: adds view selectors

--- src/client/src/selectors/items-view-selectors.js
+++ src/client/src/selectors/items-view-selectors.js
@@ -1,1 +1,5 @@
 const getItemsView = state => state.itemsView;
+
+export const getSuccessful = state => getItemsView(state).successful;
+export const getFailed = state => getItemsView(state).failed;
+export const getLoading = state => getItemsView(state).is_loading;
